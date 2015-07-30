b3bfbc5328cdb4bd0e5de699bb5e27b517d17e0b
Cole Murray <colemurray.cs@gmail.com>
2015-07-29 22:44:38 -0700

    Completed Assignment 2 100/100

---
+++ ex2/sigmoid.m
@@ -0,0 +1,24 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z)); %G can be multiple dimensions
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+%for i = 1:(length(z))
+%    for j = 1:(length(z))
+%        g(i,j) = 1 / (1 + exp(-z(i,j)));
+%    end
+%end 
+
+% Optimized =======================
+g = 1 ./ (1 + exp(-z));
+
+
+% =============================================================
+
+end
